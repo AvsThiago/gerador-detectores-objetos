788ba47fd7d7fddf5ef74659d65eb2483f752d92
AvsThiago <avsthiago@gmail.com>
2016-12-23 17:47:07 -0200

    Comentários adicionados nos métodos da classe PreProcessing

--- PreProcessing.py
+++ PreProcessing.py
@@ -1,18 +1,17 @@
 """
-
  This class provides many methods for pre processing of images,
  the main objective is to encapsulate methods that will
  be used in the main projects.
 
  @author  Thiago da Silva Alves
  @version 1.0, 23/12/16
-
 """
 
 import os
 import cv2 as cv
 
 def compare_images(image1, image2):
+    """Compare two images and return they are"""
     try:
         return image1.shape == image2.shape
                 and not(np.bitwise_xor(image1, image2).any())
@@ -20,12 +19,15 @@ def compare_images(image1, image2):
         return False
 
 def remove_file(image_path):
+    """Remove one file from it's directory"""
     os.remove(path)
 
 def rename_image(path, old_name, new_name):
+    """Rename one image from it's directory"""
     os.rename(os.path.join(path, old_name), os.path.join(path, new_name))
 
 def is_corrupted(image_path):
+    """Verify if the image passed by parameter is corrupted"""
     try:
         img_loaded = cv.imread(image_path)
         # If some error occurs when trying to fetch images' shape it's corrupted
@@ -36,12 +38,14 @@ def is_corrupted(image_path):
 
 def rename_images_path(path, prefix, out_ext=".png"
                         extensions=(".jpg", ".jpeg", ".png", ".gif", ".bmp")):
+    """Rename all images in a path, using a prefix concatenated with a number"""
     files = [fl for fl in os.listdir(path) if fl.lower().endswith(extension)]
     for i, nome in enumerate(files):
         rename_image(path, nome, ''.join([prefix, str(i), out_ext]))
 
 def remove_corrupted_img_path(path, extensions=(".jpg", ".jpeg", ".png",
                                                 ".gif", ".bmp")):
+    """Remove all corrupted images from a folder"""
     files = [fl for fl in os.listdir(path) if fl.lower().endswith(extension)]
     for nome in files:
         if is_corrupted(os.path.join(path, nome)):
@@ -50,6 +54,7 @@ def remove_corrupted_img_path(path, extensions=(".jpg", ".jpeg", ".png",
 def remove_same_images(path_images_to_compare,
                         path_images_to_be_compared
                         extensions=(".jpg", ".jpeg", ".png", ".gif", ".bmp")):
+    """Compare if two images are equal, if yes, ther second is removed"""
     to_compare = [fl for fl in os.listdir(path)
                             if fl.lower().endswith(extension)]
     to_be_compared = [fl for fl in os.listdir(path)
@@ -64,15 +69,19 @@ def remove_same_images(path_images_to_compare,
                 remove_image(dir_img2)
 
 def convert_to_gray(path, image_name):
+    """Convert an image to gray scale"""
     img = cv.imread(os.path.join(path, image_name), 0)
     cv.imwrite(path, img)
 
 def convert_to_gray_path(path, extensions=(".jpg", ".jpeg", ".png",
                                             ".gif", ".bmp")):
+    """Convert all images from a path to gray"""
     for i in os.path.listdir(path):
         convert_to_gray(path, i)
 
 def resize_image(path, image_name, max_size=500.0):
+    """Resize an image where it's bigger side will be resized to the value of
+       the parameter max_size and the another side will respect the new ratio"""
     img = cv2.imread(os.path.join(path, i))
     height, width = img.shape[:2]
     if height > width:
@@ -88,5 +97,6 @@ def resize_image(path, image_name, max_size=500.0):
 
 def resize_image_path(path, max_size, extensions=(".jpg", ".jpeg", ".png",
                                                     ".gif", ".bmp")):
+    """Resize all images of a path to a given max size."""
     for i in os.path.listdir(path):
         resize_image(path, i, max_size)
